4fed10de2d6ebb660a0164156f1a7282ebf65e88
Tarindeka <tndkpaul@gmail.com>
2025-11-06 16:01:53 +0300

    Fix PHP syntax and improve file upload handling

--- classroom_management/add_assignment.php
+++ classroom_management/add_assignment.php
@@ -1,5 +1,5 @@
 <?php
-include 'db.php',
+include 'db.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -75,4 +75,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 </body>
 </html>
+
 
